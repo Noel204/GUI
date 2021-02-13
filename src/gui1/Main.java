bf6e933eec1f4c04638f5d1fb09592c167b10c45
Noel <Noel@DESKTOP-0FQ9JN3>
2021-02-13 11:35:38 -0600

    GUI

---
+++ src/gui1/Main.java
@@ -0,0 +1,27 @@
+
+package gui1;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+   
+        JFrame frame = new JFrame();
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+        frame.setTitle("Cesba");
+        
+        frame.setResizable(true);
+        //formato DEFAULT:
+        //frame.getContentPane().setBackground(Color.red);
+        //FORMATO RGB:
+        //frame.getContentPane().setBackground(new Color (28, 26, 198));
+        //FORMATO HEXADECIMAL:
+        frame.getContentPane().setBackground(new Color (0x1AC3C6));
+        
+        
+    }
+    
+}
